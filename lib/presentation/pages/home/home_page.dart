221d159ef821cab0ee06daf602821e7ccad44ec5
jamshidmirzamaxmudov <jamshimirzamxmudov3@gmail.com>
2023-02-28 17:58:54 +0500

    -- add tabbars

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:ddd_lesson/presentation/styles/styles.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -7,11 +8,68 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  @override
+  void initState() {
+    tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        bottom: TabBar(
+          controller: tabController,
+          tabs: [
+            Tab(
+              text: "Arenda",
+            ),
+            Tab(
+              text: "Sotib olish",
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 200,
+            color: Styles.primaryColor,
+            child: Center(
+              child: Text(
+                "Container",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Expanded(
+            child: TabBarView(controller: tabController,
+                children: [
+              ListView(children: [
+                Text("Arenda"),
+                Text("Arenda1"),
+                Container(
+                  width: double.infinity,
+                  height: 200,
+                  color: Styles.primaryColor,
+                  child: Center(
+                    child: Text(
+                      "Container",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                )
+              ]),
+              Text("Sotib olish"),
+            ]),
+          ),
+        ],
+      ),
     );
   }
 }
